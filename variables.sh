12cd80128082f4c5909f9878a306d152baf0cafe
Matt Ridenour <mridenour@eveo.com>
2013-08-02 18:05:52 -0700

    newrelic file name changed format with new releases

--- variables.sh
+++ variables.sh
@@ -12,7 +12,7 @@ LIBMEMCACHED_VERSION="1.0.16"    # http://libmemcached.org/libMemcached.html
 MEMCACHED_VERSION="2.1.0"    # pecl.php.net/package/memcached
 PHP_TGZ_FILE="php-${PHP_VERSION}.tar.gz"
 
-NEWRELIC_VERSION=" 3.5.3.25"    # http://download.newrelic.com/php_agent/release/
-NEWRELIC_TGZ_FILE="newrelic-${NEWRELIC_VERSION}.tar.gz"
+NEWRELIC_VERSION=" 3.7.5.7"    # http://download.newrelic.com/php_agent/release/
+NEWRELIC_TGZ_FILE="newrelic-php5-${NEWRELIC_VERSION}-linux.tar.gz"
 
 MANIFEST_FILE="manifest.md5sum"
